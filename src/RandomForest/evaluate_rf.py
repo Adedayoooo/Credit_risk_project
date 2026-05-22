4984ecde7978b3a5ca87a51fe939797ba2394db2
Adedayo Adebayo <adebayoadedayo23@gmail.com>
2026-05-22 08:17:45 +0100

    Update evaluate_rf.py

--- src/RandomForest/evaluate_rf.py
+++ src/RandomForest/evaluate_rf.py
@@ -5,6 +5,7 @@ import logging
 import pickle
 from pathlib import Path
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s | %(levelname)s | %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger(__name__)
 
 def load_model():
